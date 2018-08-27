5122d6e5778bc1f6aa24c2c03847e1d0ec8a0f01
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-27 17:05:15 +1000

    finished app

--- shopping.rb
+++ shopping.rb
@@ -1,14 +1,32 @@
 # Grocery store array demo
 
-shopping_list = []
+shopping_list = {}
 
-puts 'Enter items to add to list (1 per line)'
-3.times do
-  new_item = gets.chomp
-  if new_item == 'ice cream'
-    new_item = 'broccoli'
+puts "Welcome to the shopping list app"
+while true
+  puts "How many items do you wish to add?"
+  number_of_items = gets.chomp.to_i
+  number_of_items.times do
+    puts 'Enter item'
+    new_item = gets.chomp
+    if new_item == 'ice cream'
+      new_item = 'broccoli'
+    end
+    puts "How many #{new_item}?"
+    qty = gets.chomp.to_i
+    # FIXME: Check if valid integer
+    qty = 0 if qty < 0
+    shopping_list[new_item] = qty
   end
-  shopping_list << new_item
+  system('clear')
+  puts "Sorted Shopping List:"
+  total_item_qty = 0
+  for item, qty in shopping_list.sort
+    puts "Item: #{item}".ljust(20) + "Qty: #{qty.to_s.rjust(3)}"
+    total_item_qty += qty
+  end
+  puts "Total Items: #{total_item_qty}"
+  puts "Add more items? (Y/N)"
+  answer = gets.chomp.downcase
+  break unless answer == 'y'
 end
-puts "Total Items: #{shopping_list.length}"
-puts shopping_list.sort!
