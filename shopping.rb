2dfe961bac103584e0be44132eb68100e775698b
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-27 15:41:26 +1000

    sort and show total qty of items

--- shopping.rb
+++ shopping.rb
@@ -6,4 +6,5 @@ puts 'Enter items to add to list (1 per line)'
 3.times do
   shopping_list << gets.chomp
 end
-puts shopping_list
+puts "Total Items: #{shopping_list.length}"
+puts shopping_list.sort
