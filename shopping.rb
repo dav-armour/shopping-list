f0b8e35bf6a15d6a059a775d068afab9f2525bc9
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-27 15:44:52 +1000

    replace ice cream with broccoli

--- shopping.rb
+++ shopping.rb
@@ -4,7 +4,11 @@ shopping_list = []
 
 puts 'Enter items to add to list (1 per line)'
 3.times do
-  shopping_list << gets.chomp
+  new_item = gets.chomp
+  if new_item == 'ice cream'
+    new_item = 'broccoli'
+  end
+  shopping_list << new_item
 end
 puts "Total Items: #{shopping_list.length}"
-puts shopping_list.sort
+puts shopping_list.sort!
